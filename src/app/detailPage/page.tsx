857d0395ac1165502ad081962e72a7977ffc546b
Alexej Kunz <alexej1@web.de>
2023-08-10 13:59:15 +0200

    Added CapsulePriceInfo

--- src/app/detailPage/page.tsx
+++ src/app/detailPage/page.tsx
@@ -1,12 +1,14 @@
 import NavHead from "@/components/NavHead/NavHead";
 import styles from "./page.module.css";
 import Card from "@/components/Card/Card";
+import CapsuleInfo from "@/components/CapsuleInfo/CapsuleInfo";
 
 function page() {
   return (
     <>
       <NavHead/>
       <Card icon="https://steamcommunity-a.akamaihd.net/economy/image/-9a81dlWLwJ2UUGcVs_nsVtzdOEdtWwKGZZLQHTxDZ7I56KU0Zwwo4NUX4oFJZEHLbXU5A1PIYQNqhpOSV-fRPasw8rsQEl9Jg9SpIW1KgRr7OPJYzRRvozkx7-HkvDxPb_CqWdQ-sJ0xL2QrI-n0QSx-BJpNWH2II6XIFA6Yl-E_1W_kOi8gp_ovsjOyXE3uyc8pSGK6iapj-s/"/>
+      <CapsuleInfo title="Paris 2023 Legends" stickerValue={70} capsulePrice={0.25}/>
     </>
   )
 }
---
+++ src/components/CapsuleInfo/CapsuleInfo.tsx
@@ -0,0 +1,27 @@
+import styles from "./CapsuleInfo.module.css";
+
+type Props = {
+    title: string,
+    stickerValue: number,
+    capsulePrice: number,
+}
+
+function CapsuleInfo({ title, stickerValue, capsulePrice }: Props) {
+    return (
+        <div className={styles.capsuleInfo}>
+            <h3 className={styles.title}>{title}</h3>
+            <div className={styles.capsulePriceInfoContainer}>
+                <div className={styles.capsulePriceInfo}>
+                    <p>Sticker Value:</p>
+                    <p>{stickerValue.toLocaleString("de-DE", { style: "currency", currency: "EUR" }).replace(",", ".")}</p>
+                </div>
+                <div className={styles.capsulePriceInfo}>
+                    <p>Capsule Price:</p>
+                    <p>{capsulePrice.toLocaleString("de-DE", { style: "currency", currency: "EUR" }).replace(",", ".")}</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default CapsuleInfo
