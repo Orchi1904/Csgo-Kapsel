5f33ca26bba09cd67843c3e944539fe8b42b140b
Alexej Kunz <alexej1@web.de>
2023-08-24 18:03:57 +0200

    Added comment

--- src/app/libs/getCapsules.tsx
+++ src/app/libs/getCapsules.tsx
@@ -1,4 +1,4 @@
-//Todo: Fix issue that dreamhack 2014 cant be found + check data on website
+//Todo: Fix issue that dreamhack 2014 cant be found + check data on website + change http icon to https or it wont work
 import { Capsule } from "@/types";
 
 export default async function getCapsules(capsules: Capsule[]) {
