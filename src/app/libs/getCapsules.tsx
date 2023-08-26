b6a0d4d01df06edd7a2bdba9a47d68229247751f
Alexej Kunz <alexej1@web.de>
2023-08-26 13:48:27 +0200

    Newest capsules are now fetched through single search

--- src/app/libs/getCapsules.tsx
+++ src/app/libs/getCapsules.tsx
@@ -1,23 +1,37 @@
-//Todo: Newest Cases have to be found through single search + check data on website + change http icon to https or it wont work
+//Todo: Check data on website + change http icon to https or it wont work
 //+ Look at Firebase Next.js Website I saved on Chrome + remove current firebase code
 import CapsuleInfo from "../../capsules.json";
 
 export default async function getCapsules() {
     const response = await fetch("http://csgobackpack.net/api/GetItemsList/v2/");
 
-    if(!response.ok){
+    if (!response.ok) {
         throw new Error("Failed to load CSGO data");
     }
 
     const csgoData = await response.json();
     const itemsList = csgoData.items_list;
 
-    CapsuleInfo.major_capsules.map((capsule) => {
+    CapsuleInfo.major_capsules.map(async (capsule) => {
         const item = itemsList[capsule.title];
-        console.log(item?.name);
+
+        if (!item) {
+            const response = await fetch(`http://csgobackpack.net/api/GetItemPrice/?id=${capsule.title}&icon=1`);
+
+            if (!response.ok) {
+                throw new Error("Failed to load CSGO data");
+            }
+
+            const notListedItem = await response.json();
+            console.log(capsule.title);
+            console.log(notListedItem.average_price);
+        } else {
+
+            console.log(item?.name);
+        }
     })
 
-   
+
 
 
     //Promise.all is used so that all data fetching starts at once and will wait unitll all fetching is done
