1678ab7510bf53a64a7ab0579a681b4d730199d5
Alexej Kunz <alexej1@web.de>
2023-09-10 22:29:55 +0200

    Improved some CSS

--- src/app/libs/getCapsules.tsx
+++ src/app/libs/getCapsules.tsx
@@ -1,5 +1,5 @@
-/*Todo: globals.css nach unnötigen Sachen durchsuchen + evtl. schauen wieso Card-Größe sich verändert
-        wenn scroll da ist, vs wenn nicht + Search Aufgabe abschließen auf jira
+/*Todo: Schauen wieso Card-Größe sich verändert wenn scroll da ist, vs wenn nicht 
+        + Search Aufgabe abschließen auf jira
         
         Request for all csgo data takes up about 10s, but vercel only allows max 5s -> Website doesnt work if data on firebase is older than 8h
         -> Fix this maybe with search route
