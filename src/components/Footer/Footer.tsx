e9b90df3e072a896e9c26240e907d6f1a00dd6a5
Alexej Kunz <alexej1@web.de>
2023-09-13 15:46:34 +0200

    Fixed footer width

--- src/components/Footer/Footer.tsx
+++ src/components/Footer/Footer.tsx
@@ -15,34 +15,36 @@ type Props = {
 function Footer({ accentColor }: Props) {
     const hoverStyle = {
         "--hoverColor": accentColor,
-        transition: "color 0.3s, fill 0.3s",  
+        transition: "color 0.3s, fill 0.3s",
     }
 
     return (
         <footer className={styles.footer}>
-            <h3 className={styles.footerSlogan}>
-                Made with
-                <span className={styles.heartIconContainer}>
-                    <FavoriteBorderIcon className={styles.heartIcon} style={{ color: accentColor }} />
-                </span>
-                by Orchi1904
-            </h3>
-            <div className={styles.contactContainer}>
-                <h4 className={styles.contactHeadline}>Contact me</h4>
-                <div className={styles.contactIconContainer}>
-                    <Link href={"https://www.linkedin.com/in/alexej-kunz/"}>
-                        <LinkedInIcon className={`${styles.contactIcon} ${styles.linkedInIcon}`}
-                            style={hoverStyle} />
-                    </Link>
-                    <Link href={"https://github.com/Orchi1904"}>
-                        <GitHubIcon className={styles.contactIcon}
-                            style={hoverStyle} />
-                    </Link>
-                    <Link href={"https://www.frontendmentor.io/profile/Orchi1904"}>
-                        <div className={styles.contactIcon}>
-                            <FrontendMentorIcon style={hoverStyle} fill="white"/>
-                        </div>
-                    </Link>
+            <div className={styles.footerContent}>
+                <h3 className={styles.footerSlogan}>
+                    Made with
+                    <span className={styles.heartIconContainer}>
+                        <FavoriteBorderIcon className={styles.heartIcon} style={{ color: accentColor }} />
+                    </span>
+                    by Alexej
+                </h3>
+                <div className={styles.contactContainer}>
+                    <h4 className={styles.contactHeadline}>Contact me</h4>
+                    <div className={styles.contactIconContainer}>
+                        <Link href={"https://www.linkedin.com/in/alexej-kunz/"}>
+                            <LinkedInIcon className={`${styles.contactIcon} ${styles.linkedInIcon}`}
+                                style={hoverStyle} />
+                        </Link>
+                        <Link href={"https://github.com/Orchi1904"}>
+                            <GitHubIcon className={styles.contactIcon}
+                                style={hoverStyle} />
+                        </Link>
+                        <Link href={"https://www.frontendmentor.io/profile/Orchi1904"}>
+                            <div className={styles.contactIcon}>
+                                <FrontendMentorIcon style={hoverStyle} fill="white" />
+                            </div>
+                        </Link>
+                    </div>
                 </div>
             </div>
         </footer >
