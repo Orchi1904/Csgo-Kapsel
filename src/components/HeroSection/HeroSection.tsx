994c2c82a36bd14d01ed56c5b1c2b4602d41cf7c
Alexej Kunz <alexej1@web.de>
2023-09-14 10:38:19 +0200

    Improved Hero Section a bit

--- src/components/HeroSection/HeroSection.tsx
+++ src/components/HeroSection/HeroSection.tsx
@@ -4,12 +4,6 @@ import styles from "./HeroSection.module.css";
 function HeroSection() {
     return (
         <div className={styles.heroContainer}>
-            <Image
-                className={styles.heroImage}
-                src="/images/hero.svg"
-                alt="Get Price Information For All CS Major Sticker Capsules"
-                fill
-            />
             <h1 className={styles.heroText}>Get Price Information For All CS Major Sticker Capsules</h1>
         </div>
     )
