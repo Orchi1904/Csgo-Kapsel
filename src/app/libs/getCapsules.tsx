3e31f0acfd4146b9bbb26fb93f127f3ca4fc94b6
Alexej Kunz <alexej1@web.de>
2023-09-19 11:29:20 +0200

    Possibly fixed Vercel API issue

--- src/app/libs/getCapsules.tsx
+++ src/app/libs/getCapsules.tsx
@@ -1,7 +1,6 @@
-/*Todo: 
+/*Todo: Csgobackpack-API request should now only use the search route because of the vercel issue
+
         Vlt CapsuleInfo auch verwenden für die Capsule bzw. zumindest Tooltip als Komponente exportieren
-        Request for all csgo data takes up about 10s, but vercel only allows max 5s -> Website doesnt work if data on firebase is older than 8h
-        -> Fix this maybe with search route
         Check if newest data will be instantly shown on website when older than 8h
 */
 
@@ -20,54 +19,35 @@ export default async function getCapsules(): Promise<CapsuleData[]> {
 
     /*Fetch new data if data is older than 8 hours, because 
       csgobackpack api updates every 8 hours*/
-    /*if (timeStampHoursDiff > 8) {
+    if (timeStampHoursDiff > 8) {
         await updateCapsulesFB();
         return await getCapsulesFB();
-    } else {*/
-    return capsuleDataFB;
-    //}
+    } else {
+        return capsuleDataFB;
+    }
 }
 
 
 async function updateCapsulesFB() {
-    const response = await fetch("http://csgobackpack.net/api/GetItemsList/v2/");
-
-    if (!response.ok) {
-        throw new Error("Failed to load CSGO data");
-    }
-
-    const csgoData = await response.json();
-    const itemsList = csgoData.items_list;
-
     let average_price: number | "N/A", icon: string, currency: string;
 
     for (const capsule of Capsules.major_capsules) {
-        const item = itemsList[capsule.title];
+        const response = await fetch(`http://csgobackpack.net/api/GetItemPrice/?id=${capsule.title}&icon=1`);
 
-        //Some capsules are not listed in the response so we have to fetch from another route
-        if (!item) {
-            const response = await fetch(`http://csgobackpack.net/api/GetItemPrice/?id=${capsule.title}&icon=1`);
-
-            if (!response.ok) {
-                throw new Error("Failed to load CSGO data");
-            }
-
-            const notListedCapsule = await response.json();
-
-            average_price = notListedCapsule.average_price ? Number(notListedCapsule.average_price) : "N/A";
-            icon = "https://steamcommunity.com/economy/image/" + notListedCapsule.icon.replace("http://cdn.steamcommunity.com/economy/image/", "").slice(0, -1);
-            currency = notListedCapsule.currency;
-        } else {
-            average_price = item.price["7_days"]?.average ? Number(item.price["7_days"].average) : "N/A";
-            icon = "https://steamcommunity.com/economy/image/" + item.icon_url;
-            currency = csgoData.currency;
+        if (!response.ok) {
+            throw new Error("Failed to load CSGO data");
         }
 
-        const stickerArr: Sticker[] = await getStickers(capsule, itemsList);
+        const capsuleItem = await response.json();
+
+        average_price = capsuleItem.average_price ? Number(capsuleItem.average_price) : "N/A";
+        icon = "https://steamcommunity.com/economy/image/" + capsuleItem.icon.replace("http://cdn.steamcommunity.com/economy/image/", "").slice(0, -1);
+        currency = capsuleItem.currency;
+
+        const stickerArr: Sticker[] = await getStickers(capsule);
 
         const stickerValue = calculateStickerValue(stickerArr);
-        const svpRatio = stickerValue !== "N/A" && average_price !== "N/A" ? stickerValue/average_price : "N/A";
-        console.log(svpRatio);
+        const svpRatio = stickerValue !== "N/A" && average_price !== "N/A" ? stickerValue / average_price : "N/A";
 
         const capsuleData: CapsuleData = {
             name: capsule.title,
--- src/app/libs/getStickers.tsx
+++ src/app/libs/getStickers.tsx
@@ -1,36 +1,22 @@
 import { Capsule, Sticker } from "@/types";
 
-export default async function getStickers(capsule: Capsule, itemsList: any): Promise<Sticker[]> {
+export default async function getStickers(capsule: Capsule): Promise<Sticker[]> {
     const stickerArr: Sticker[] = [];
-    let price: number | "N/A" = "N/A"; 
+    let price: number | "N/A" = "N/A";
 
     for (const sticker of capsule.stickers) {
-        const stickerItem = itemsList["Sticker | " + sticker.name];
+        const response = await fetch(`http://csgobackpack.net/api/GetItemPrice/?id=Sticker | ${sticker}&icon=1`);
 
-        //Fetching data from another route because some stickers are not in current route response
-        if (!stickerItem) {
-            const response = await fetch(`http://csgobackpack.net/api/GetItemPrice/?id=Sticker | ${sticker}&icon=1`);
+        if (!response.ok) {
+            throw new Error("Failed to load CSGO data");
+        }
 
-            if (!response.ok) {
-                throw new Error("Failed to load CSGO data");
-            }
+        const stickerItem = await response.json();
 
-            const differentRouteSticker = await response.json();
-
-            if (differentRouteSticker.success === true) {
-                price = differentRouteSticker.average_price !== 0 ? Number(differentRouteSticker.average_price) : "N/A";
-            } else {
-                price = "N/A";
-            }
+        if (stickerItem.success === true) {
+            price = stickerItem.average_price !== 0 ? Number(stickerItem.average_price) : "N/A";
         } else {
-            //Have to do that because sometimes there is only 30_days available
-            if (stickerItem.price["24_hours"]) {
-                price = stickerItem.price["24_hours"].average;
-            } else if (stickerItem.price["7_days"]) {
-                price = stickerItem.price["7_days"].average;
-            } else if (stickerItem.price["30_days"]) {
-                price = stickerItem.price["30_days"].average;
-            }
+            price = "N/A";
         }
 
         stickerArr.push({
