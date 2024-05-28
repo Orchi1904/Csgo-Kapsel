7e076a1f89dffec00dcfe05d170daeda0a3a8e7c
Alexej Kunz <alexej1@web.de>
2024-05-28 22:15:24 +0200

    Quickfixed API problem by using static firebase data

--- src/libs/getCapsules.tsx
+++ src/libs/getCapsules.tsx
@@ -2,92 +2,100 @@ import getStickers from "./getStickers";
 import Capsules from "../helper/capsules.json";
 import { CapsuleData, Sticker } from "@/types";
 import { getCapsulesFB } from "@/firebase/firestore/getCapsules";
-import { headers } from 'next/headers';
+import { headers } from "next/headers";
 
 export default async function getCapsules(): Promise<CapsuleData[]> {
-    //Fixes the static page problem, otherwise updating data will not work on builded page
-    const headersList = headers();
+  //Fixes the static page problem, otherwise updating data will not work on builded page
+  // Todo: Replace with revalidate, after API is working again
+  const headersList = headers();
 
-    const capsuleDataFB: CapsuleData[] = await getCapsulesFB();
-    const lastUpdatedTimestampHours = capsuleDataFB[0].last_updated / 1000 / 3600;
-    const currentTimeStampHours = new Date().getTime() / 1000 / 3600;
-    const timeStampHoursDiff = currentTimeStampHours - lastUpdatedTimestampHours;
+  const capsuleDataFB: CapsuleData[] = await getCapsulesFB();
+  const lastUpdatedTimestampHours = capsuleDataFB[0].last_updated / 1000 / 3600;
+  const currentTimeStampHours = new Date().getTime() / 1000 / 3600;
+  const timeStampHoursDiff = currentTimeStampHours - lastUpdatedTimestampHours;
 
-    /*Fetch new data if data is older than 8 hours, because 
-      csgobackpack api updates every 8 hours*/
-    if (timeStampHoursDiff > 8) {
+  /*Fetch new data if data is older than 8 hours, because 
+      csgobackpack api updates every 8 hours
+      -> As of May 28th 2024, the data is only fetched from firebase and not from 
+         csgobackpack API anymore, because it seems that this API was removed. 
+         Todo: Replace Csgobackpack API with Steam-Links in JSON-Format:
+         https://stackoverflow.com/questions/31375860/retrieve-all-csgo-items-via-steam-api-or-endpoint*/
+  /* if (timeStampHoursDiff > 8) {
         return await updateCapsules();
-    } else {
-        return capsuleDataFB;
-    }
+    } else { */
+  return capsuleDataFB;
+  // }
 }
-
 
 async function updateCapsules() {
-    const response = await fetch("http://csgobackpack.net/api/GetItemsList/v2/?currency=EUR&extend=1");
-    const capsuleArr: CapsuleData[] = [];
+  const response = await fetch(
+    "http://csgobackpack.net/api/GetItemsList/v2/?currency=EUR&extend=1"
+  );
+  const capsuleArr: CapsuleData[] = [];
 
-    if (!response.ok) {
-        throw new Error("Failed to load CSGO data");
+  if (!response.ok) {
+    throw new Error("Failed to load CSGO data");
+  }
+
+  const csgoData = await response.json();
+  const itemsList = csgoData.items_list;
+
+  let average_price: number | "N/A", icon: string, currency: string;
+
+  for (const capsule of Capsules.major_capsules) {
+    const item = itemsList[capsule.title];
+
+    if (item.price["24_hours"]) {
+      average_price = item.price["24_hours"].average;
+    } else if (item.price["7_days"]) {
+      average_price = item.price["7_days"].average;
+    } else if (item.price["30_days"]) {
+      average_price = item.price["30_days"].average;
+    } else if (item.price["all_time"].average) {
+      average_price = item.price["all_time"].average;
+    } else {
+      average_price = "N/A";
     }
 
-    const csgoData = await response.json();
-    const itemsList = csgoData.items_list;
+    icon = "https://steamcommunity.com/economy/image/" + item.icon_url;
+    currency = csgoData.currency;
 
-    let average_price: number | "N/A", icon: string, currency: string;
+    const stickerArr: Sticker[] = await getStickers(capsule, itemsList);
+    const stickerValue = calculateStickerValue(stickerArr);
+    const svpRatio =
+      stickerValue !== "N/A" && average_price !== "N/A"
+        ? stickerValue / average_price
+        : "N/A";
 
-    for (const capsule of Capsules.major_capsules) {
-        const item = itemsList[capsule.title];
-
-        if (item.price["24_hours"]) {
-            average_price = item.price["24_hours"].average;
-        } else if (item.price["7_days"]) {
-            average_price = item.price["7_days"].average;
-        } else if (item.price["30_days"]) {
-            average_price = item.price["30_days"].average;
-        } else if (item.price["all_time"].average) {
-            average_price = item.price["all_time"].average;
-        } else {
-            average_price = "N/A";
-        }
-
-        icon = "https://steamcommunity.com/economy/image/" + item.icon_url;
-        currency = csgoData.currency;
-
-        const stickerArr: Sticker[] = await getStickers(capsule, itemsList);
-        const stickerValue = calculateStickerValue(stickerArr);
-        const svpRatio = stickerValue !== "N/A" && average_price !== "N/A" ? stickerValue / average_price : "N/A";
-
-        const capsuleData: CapsuleData = {
-            name: capsule.title,
-            average_price: average_price !== 0 ? average_price : "N/A",
-            icon,
-            currency,
-            steam_link: `https://steamcommunity.com/market/listings/730/${capsule.title}`,
-            sticker_value: stickerValue,
-            svp_ratio: svpRatio,
-            stickers: stickerArr,
-            last_updated: 0,
-        }
-        capsuleArr.push(capsuleData);
-    }
-    return capsuleArr;
+    const capsuleData: CapsuleData = {
+      name: capsule.title,
+      average_price: average_price !== 0 ? average_price : "N/A",
+      icon,
+      currency,
+      steam_link: `https://steamcommunity.com/market/listings/730/${capsule.title}`,
+      sticker_value: stickerValue,
+      svp_ratio: svpRatio,
+      stickers: stickerArr,
+      last_updated: 0,
+    };
+    capsuleArr.push(capsuleData);
+  }
+  return capsuleArr;
 }
 
-
 function calculateStickerValue(stickerArr: Sticker[]): number | "N/A" {
-    let stickerValue: number | "N/A" = 0;
+  let stickerValue: number | "N/A" = 0;
 
-    stickerArr.map((sticker) => {
-        if (sticker.average_price === "N/A") {
-            stickerValue = "N/A";
-            return;
-        }
+  stickerArr.map((sticker) => {
+    if (sticker.average_price === "N/A") {
+      stickerValue = "N/A";
+      return;
+    }
 
-        if (stickerValue !== "N/A") {
-            stickerValue += sticker.average_price;
-        }
-    });
+    if (stickerValue !== "N/A") {
+      stickerValue += sticker.average_price;
+    }
+  });
 
-    return stickerValue;
+  return stickerValue;
 }
